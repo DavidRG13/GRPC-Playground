65ddf892d622b8ec7f9735cc06d208fee710cad7
David Rodriguez <david.rodriguez@williamhill.com>
2017-09-19 10:46:13 +0200

    Split simulations

---
+++ perftest/src/test/scala/gatling/simulations/JSONGetEmployeeSimulation.scala
@@ -0,0 +1,24 @@
+package gatling.simulations
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+import scala.util.Random
+
+class JSONGetEmployeeSimulation extends Simulation {
+  val feeder = Iterator.continually(Map("id" -> Random.nextInt()))
+
+  val httpProtocol = http
+    .baseURL(s"http://localhost:8080")
+
+  val scn = scenario("getEmployee json load test")
+      .feed(feeder)
+      .exec(http("json").get("/employee/${id}").header("Accept", "application/json").check(regex(".*id.*")))
+
+  setUp(
+    scn.inject(
+      rampUsers(5000) over (5 seconds)
+    )
+  ).protocols(httpProtocol)
+}
--- perftest/src/test/scala/gatling/simulations/ProtoGetEmployeeSimulation.scala
+++ perftest/src/test/scala/gatling/simulations/ProtoGetEmployeeSimulation.scala
@@ -5,26 +5,18 @@ import io.gatling.http.Predef._
 import scala.concurrent.duration._
 import scala.util.Random
 
-class GetEmployeeSimulation extends Simulation {
+class ProtoGetEmployeeSimulation extends Simulation {
   val feeder = Iterator.continually(Map("id" -> Random.nextInt()))
 
   val httpProtocol = http
     .baseURL(s"http://localhost:8080")
 
-  val scn = scenario("getEmployee json load test")
-      .feed(feeder)
-      .exec(http("json").get("/employee/${id}").header("Accept", "application/json").check(regex(".*id.*")))
-  val scn2 = scenario("getEmployee proto load test")
+  val scn = scenario("getEmployee proto load test")
       .feed(feeder)
       .exec(http("proto").get("/employee/${id}").header("Accept", "application/x-protobuf").check(status.is(200)))
 
-
-
   setUp(
     scn.inject(
-      rampUsers(5000) over (5 seconds)
-    ),
-    scn2.inject(
       rampUsers(5000) over (5 seconds)
     )
   ).protocols(httpProtocol)
