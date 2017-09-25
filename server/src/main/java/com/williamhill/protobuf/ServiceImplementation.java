4ca71d9878cca5a702c5a4b5bd810a259a67b46a
david <DAVIDrg?>
2017-09-25 20:29:21 +0200

    Adding error handling

--- server/src/main/java/com/williamhill/protobuf/ServiceImplementation.java
+++ server/src/main/java/com/williamhill/protobuf/ServiceImplementation.java
@@ -1,5 +1,7 @@
 package com.williamhill.protobuf;
 
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 
 // Bad named to make it clearer
@@ -10,6 +12,12 @@ public class ServiceImplementation extends EmployeesServiceGrpc.EmployeesService
         int employeeId = request.getId();
         System.out.println("employeeId = " + employeeId);
         EmployeeService.Employee employee = EmployeeService.Employee.newBuilder().setFirstname("Pepe").setLastname("Pepe").setId(employeeId).setSalary(1000.0).build();
+
+        if (employeeId < 0) {
+            StatusRuntimeException e = Status.INVALID_ARGUMENT.withDescription("employeeId should be positive").asRuntimeException();
+            responseObserver.onError(e);
+            //throw e;
+        }
 
         responseObserver.onNext(employee);
         responseObserver.onCompleted();
